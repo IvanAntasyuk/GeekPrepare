b7eeba6e5f2760564f50ec32ffd3f930d15f8ea3
IvanAntasyuk <Pikin64@gmail.com>
2022-02-04 19:53:26 +0300

    hibernate

---
+++ hibernate/src/main/java/services/EntityConnector.java
@@ -0,0 +1,13 @@
+package services;
+
+import org.hibernate.cfg.Configuration;
+import javax.persistence.EntityManagerFactory;
+
+public class EntityConnector {
+
+    public static EntityManagerFactory getEmFactory(){
+        return new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+    }
+}
